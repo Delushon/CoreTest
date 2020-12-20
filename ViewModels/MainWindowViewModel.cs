2fb3a1f3abee67510967189a84f092793ff13814
AlexanderP <panshin.alexander@yahoo.com>
2020-12-20 21:14:04 +0300

    test

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,10 +1,29 @@
-﻿using System;
+﻿using CoreTest.ViewModels.Base;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace CoreTest.ViewModels
 {
-    class MainWindowViewModel
+    internal class MainWindowViewModel : ViewModel
     {
+        #region Заголовок окна
+
+        private string _Title = "Пробный Title";
+
+        /// <summary>Заголовок окна</summary>
+        public string Title
+        {
+            get => _Title;
+            //set
+            //{
+            //    if (Equals(_Title, value)) return;
+            //    _Title = value;
+            //    OnPropertyChanged();
+            //}
+            set => Set(ref _Title, value);
+        }
+
+        #endregion
     }
 }
